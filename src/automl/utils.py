870ad29cb35d64fccf84ca4358ac2edb3f64010f
alperari <alper.hamza.ari@gmail.com>
2024-08-05 19:34:33 +0200

    refactor: :recycle: add data augmentation transforms

--- src/automl/utils.py
+++ src/automl/utils.py
@@ -160,6 +160,7 @@ def get_transform(
         std: float = 0.5,
         num_channels: int = None
 ):
+    # By default, resize the image to 224x224 because of MobileNetV2
     transform_list = [
         transforms.Resize((224, 224)),
     ]
@@ -168,44 +169,39 @@ def get_transform(
     if num_channels is not None and num_channels != 3:
         transform_list.append(GrayscaleToRGB())
 
-    # if config:
-    #     # If config is given, return the transform with the specifig augmentations
-    #     transform_list.extend([
-    #         transforms.Resize((224, 224)),
-    #         transforms.RandomHorizontalFlip(
-    #             p=config["random_horizontal_flip_prob"]
-    #         ),
-    #         transforms.RandomVerticalFlip(
-    #             p=config["random_vertical_flip_prob"]
-    #         ),
-    #         transforms.RandomApply([transforms.RandomRotation(
-    #             config["random_rotation_deg"])],
-    #             p=config["random_rotation_prob"]
-    #         ),
-    #         transforms.RandomApply(
-    #             [
-    #                 AddGaussianNoise(
-    #                     config["random_gaussian_noise_mean"],
-    #                     config["random_gaussian_noise_std"])
-    #             ],
-    #             p=config["random_gaussian_noise_prob"]
-    #         ),
-    #         transforms.RandomApply(
-    #             [
-    #                 transforms.ColorJitter(
-    #                     brightness=config["brightness"],
-    #                     contrast=config["contrast"],
-    #                     saturation=config["saturation"])
-    #             ], p=0.5
-    #         ),  # Assume a fixed probability for ColorJitter
-    #     ])
+    if config:
+        # If config is given, return the transform with the specifig augmentations
+        transform_list.extend([
+            transforms.RandomHorizontalFlip(
+                p=config["random_horizontal_flip_prob"]
+            ),
+            transforms.RandomVerticalFlip(
+                p=config["random_vertical_flip_prob"]
+            ),
+            transforms.RandomApply([transforms.RandomRotation(
+                config["random_rotation_deg"])],
+                p=config["random_rotation_prob"]
+            ),
+            transforms.RandomApply(
+                [
+                    AddGaussianNoise(
+                        config["random_gaussian_noise_mean"],
+                        config["random_gaussian_noise_std"])
+                ],
+                p=config["random_gaussian_noise_prob"]
+            ),
+            transforms.RandomApply(
+                [
+                    transforms.ColorJitter(
+                        brightness=config["brightness"],
+                        contrast=config["contrast"],
+                        saturation=config["saturation"])
+                ], p=0.5
+            ),  # Assume a fixed probability for ColorJitter
+        ])
 
     transform_list.append(transforms.ToTensor())
     transform_list.append(transforms.Normalize(mean=mean, std=std))
-
-    # transform_list = [
-    #     transforms.ToTensor(),
-    # ]
 
     return transforms.Compose(transform_list)
 
