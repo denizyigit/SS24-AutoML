779ff2cff2a420431d0bf9b5b57ecc71fb2ff7a6
alperari <alper.hamza.ari@gmail.com>
2024-08-06 12:00:07 +0200

    fix: :bug: avoid printing when scheduler is called on each batch

--- src/automl/scheduler_wrapper.py
+++ src/automl/scheduler_wrapper.py
@@ -15,7 +15,12 @@ class LRSchedulerWrapper:
         self.scheduler.step(epoch)
 
         current_lr = self.get_last_lr()[0]
-        if self.verbose and current_lr != self.config["learning_rate"]:
+
+        # Print the updated  learning rate if and only if:
+        # 1. The learning rate has been updated
+        # 2. The verbose flag is set to True
+        # 3. The epoch is not None (i.e., the scheduler is not being called on each batch but epoch, otherwise it would print too many times)
+        if self.verbose and current_lr != self.config["learning_rate"] and epoch is not None:
             print(
                 f"PID_{self.config['pid']}: Scheduler: Updated learning rate to : {current_lr}")
 
