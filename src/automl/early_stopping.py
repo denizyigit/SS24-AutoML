4e6799a8ce24aea61ef60474389750fe1f772502
alperari <alper.hamza.ari@gmail.com>
2024-08-06 05:46:08 +0200

    feat: :sparkles: add print statements in EarlyStopping class

--- src/automl/early_stopping.py
+++ src/automl/early_stopping.py
@@ -2,18 +2,24 @@ import numpy as np
 
 
 class EarlyStopping:
-    def init(self, patience=5, delta=0):
+    def __init__(self, patience=7, delta=0, verbose=False, pid=None):
         self.patience = patience
         self.delta = delta
+        self.verbose = verbose
+        self.pid = pid
         self.counter = 0
         self.best_loss = np.Inf
         self.early_stop = False
 
-    def call(self, val_loss):
+    def __call__(self, val_loss):
         if val_loss < self.best_loss - self.delta:
             self.best_loss = val_loss
             self.counter = 0
         else:
             self.counter += 1
+            if self.verbose:
+                print(
+                    f"PID_{self.pid}:EarlyStopping: Validation score increased! Counter: {self.counter}/{self.patience}")
             if self.counter >= self.patience:
+                print(f"PID_{self.pid}:EarlyStopping: Stopping training!")
                 self.early_stop = True
