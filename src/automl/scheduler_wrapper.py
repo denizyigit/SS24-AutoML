1407a68a9335c190c0c49a60a561746ad03a2fa2
alperari <alper.hamza.ari@gmail.com>
2024-08-06 12:21:37 +0200

    fix: :bug: correct scheduler verbose logic

--- src/automl/scheduler_wrapper.py
+++ src/automl/scheduler_wrapper.py
@@ -7,6 +7,7 @@ class LRSchedulerWrapper:
         self.scheduler = scheduler
         self.config = config
         self.verbose = verbose
+        self.last_lr = config["learning_rate"]
 
         print(
             f"PID_{self.config['pid']}: Scheduler: Initialized with {self.scheduler.__class__.__name__} and learning rate: {config['learning_rate']}")
@@ -16,13 +17,16 @@ class LRSchedulerWrapper:
 
         current_lr = self.get_last_lr()[0]
 
-        # Print the updated  learning rate if and only if:
-        # 1. The learning rate has been updated
-        # 2. The verbose flag is set to True
-        # 3. The epoch is not None (i.e., the scheduler is not being called on each batch but epoch, otherwise it would print too many times)
-        if self.verbose and current_lr != self.config["learning_rate"] and epoch is not None:
-            print(
-                f"PID_{self.config['pid']}: Scheduler: Updated learning rate to : {current_lr}")
+        if self.last_lr != current_lr:
+            self.last_lr = current_lr
+
+            # Print the updated  learning rate if and only if:
+            # 1. The learning rate has been updated
+            # 2. The verbose flag is set to True
+            # 3. The epoch is not None (i.e., the scheduler is not being called on each batch but epoch, otherwise it would print too many times)
+            if self.verbose and epoch is not None:
+                print(
+                    f"PID_{self.config['pid']}: Scheduler: Updated learning rate to : {current_lr}")
 
     def get_last_lr(self):
         return self.scheduler.get_last_lr()
